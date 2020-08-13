5276c36f2f0aa6138149608a52704fe67db0bcd2
A. Bond-Harris <abondharris@gmail.com>
2020-08-12 19:07:36 -0500

    testing repo

--- Assets/CharController.cs
+++ Assets/CharController.cs
@@ -7,7 +7,7 @@ public class CharController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //just putting in some stuff to make sure the repo is working
     }
 
     // Update is called once per frame

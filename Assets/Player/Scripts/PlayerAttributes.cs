5e8bab8717632cb7ea965474d9dee8930a0a05ce
A. Bond-Harris <abondharris@gmail.com>
2020-09-14 23:12:06 -0500

    added player attribute script
    haha, depression messed me up and I did basically nothing, sorry future me

---
+++ Assets/Player/Scripts/PlayerAttributes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttributes : MonoBehaviour
+{
+    public int health { get; private set; }
+    public int maxHealth { get; private set; }
+    public int mana { get; private set; }
+    public int maxMana { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: Load Max Health
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMaxHealth(int value)
+    {
+        maxHealth = Mathf.Max(value,1);
+    }
+}
